62a42d4e48b839da45a391b0a9606b7ec7de3ebb
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-06-13 06:35:17 -0300

    fix the error node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const points = require("./schema.js");
 const bodyParser = require('body-parser');
-const db = require("./repository.js");
+const db = require("./serverController.js");
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = "mongodb://localhost:27017/restoration";
@@ -13,29 +13,27 @@ console.log('connect to mongodb')
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/points", (req, res) => {
-    points.find((error, response) => {
-        // preciso tratar o erro, caso ocorra
-        if (error) {
-            // caso tenha algum erro
-            return res.status(500).send(error);
-        }
-        // caso contrário, envio o retorno
-        res.status(200).send(response);
-    });
+app.get("/", (req, res) => {
+    const hello = `
+    <pre>
+        Este é  o Geotech database
+        where you can see/use geographical information (coordinate and EVI2 index value)
+        of some random points found and tag as "potencial 
+        restoration areas in Minas Gerais/Brazil", using the libray geospatial to
+        reconstruct spectral trajectory of a pixel "Landtrend" .
+
+        GET/points == 
+        DELETE/poinst/:id
+        POST/points{slug_name, coordinates(long/lat), data (' may6/1990 , EVI2LTGEE: index)}
+    </pre>
+    `
+
+    res.send(hello)
 })
 
-app.get("/points/:id", (req, res) => {
-    points.findById(
-        req.params.id,
 
-        function(err, point) {
-            if (err) return res.send(err);
+app.get("/points", (req, res) => {
+    res.send(controller.getAll())
+})
 
-            if (!point) return res.status(404).send({});
-            console.log('*** point.id:', point.id)
-            res.send(point);
-        }
-    );
-});
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}...`));
