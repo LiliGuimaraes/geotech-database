33c90c1f2237f40529f4f14c10487465bb6cc8f9
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-06-15 13:29:30 -0300

    fix bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const points = require("./schema.js");
 const bodyParser = require('body-parser');
-const db = require("./serverController")
+const db = require("./serverController.js")
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = "mongodb://localhost:27017/restoration";
@@ -12,7 +12,7 @@ console.log('connect to mongodb')
 
 const app = express();
 app.use(bodyParser.json());
-
+console.log(bodyParser)
 app.get("/", (req, res) => {
     const hello = `
     <pre>
@@ -42,70 +42,78 @@ app.get("/points", (req, res) => {
     });
 })
 
-
 app.get("/points/:id", (req, res) => {
     points.findById(
         req.params.id,
-
         function(err, point) {
             if (err) return res.send(err);
 
             if (!point) return res.status(404).send({});
-            console.log('*** maravilhosa.id:', point.id)
+            console.log('*** point.id:', point.id)
             res.send(point);
         }
     );
 });
 
-app.post("/points", (req, res) => {
-    const newPoint = new maravilhosas({
+app.get("/new", (req, res) => {
+    res.render("new");
+});
+
+app.post("/new", (req, res) => {
+    const newPoint = new points({
         point_slug: req.body.point_slug,
         coordinates: req.body.coordinates,
         data: req.body.data,
     });
 
-    newPoint.save(err => {
-        if (err) return res.send(err);
-        res.send(newPoint);
-    });
+    var data = new points(newPoint)
+    data.save();
+
+    res.redirect('/');
 });
 
-
-app.put("/points/:id", (req, res) => {
-    points.findByIdAndUpdate(
-        req.params.id, { $set: req.body }, { new: true },
-        function(error, point) {
-            if (error)
-                return res.status(error.code).send(error.message);
-            res.send(point);
-        }
-    );
-})
-
-app.patch("/points/:id", (req, res) => {
-    points.findByIdAndUpdate(
-        req.params.id, { $set: req.body }, { new: true },
-        function(error, point) {
-            if (error) {
-                let errorCode = error.code
-                if (!error.code) {
-                    errorCode = 400 // Bad Request
-                }
-                return res.status(errorCode).send(error.message);
-            }
-
-            res.send(point);
-        }
-    );
-})
-
-app.delete("/points/:id", (req, res) => {
-    points.findByIdAndDelete(
-        req.params.id, { $set: req.body },
-        function(err, point) {
-            if (err) return res.status(err.code).send(err.message);
-            res.send(point);
-        }
-    );
+app.get('/', function(req, res, next) {
+    points.find()
+        .then(function(doc) {
+            res.render('server', { datas: doc });
+        });
 });
+
+// app.put("/points/:id", (req, res) => {
+//     points.findByIdAndUpdate(
+//         req.params.id, { $set: req.body }, { new: true },
+//         function(error, point) {
+//             if (error)
+//                 return res.status(error.code).send(error.message);
+//             res.send(point);
+//         }
+//     );
+// })
+
+// app.patch("/points/:id", (req, res) => {
+//     points.findByIdAndUpdate(
+//         req.params.id, { $set: req.body }, { new: true },
+//         function(error, point) {
+//             if (error) {
+//                 let errorCode = error.code
+//                 if (!error.code) {
+//                     errorCode = 400 // Bad Request
+//                 }
+//                 return res.status(errorCode).send(error.message);
+//             }
+
+//             res.send(point);
+//         }
+//     );
+// })
+
+// app.delete("/points/:id", (req, res) => {
+//     points.findByIdAndDelete(
+//         req.params.id, { $set: req.body },
+//         function(err, point) {
+//             if (err) return res.status(err.code).send(err.message);
+//             res.send(point);
+//         }
+//     );
+// });
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}...`));
